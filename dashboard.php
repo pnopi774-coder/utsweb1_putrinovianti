a99bf7e3c354eb665cf9fb2f842f14b117a97b43
pnopi774-coder <pnopi774@gmail.com>
2025-11-06 13:40:29 +0700

    commit 5

--- dashboard.php
+++ dashboard.php
@@ -24,18 +24,10 @@ t
 
 </html>
 
-$beli = [];
-$jumlah = [];
-$total = [];
+echo "<h2>--POLGAN MART--</h2>";
+echo "<p>Selamat datang, " . $_SESSION['username'] . "!</p><hr>";
 
-for ($i = 0; $i < count($kode_barang); $i++) {
-    // Random apakah barang dibeli (0 atau 1)
-    $beli[$i] = rand(0, 1);
-    if ($beli[$i] == 1) {
-        $jumlah[$i] = rand(1, 5); // jumlah acak 1–5
-        $total[$i] = $harga_barang[$i] * $jumlah[$i];
-    } else {
-        $jumlah[$i] = 0;
-        $total[$i] = 0;
-    }
-}
+// Data produk (array)
+$kode_barang = ["B001", "B002", "B003", "B004", "B005"];
+$nama_barang = ["Sabun", "Sampo", "Pasta Gigi", "Tisu", "Detergen"];
+$harga_barang = [5000, 12000, 8000, 7000, 15000];
